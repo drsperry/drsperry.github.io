48e62cb216a6fa92057f31fb6aeb98659502a34d
mathough <mathough@gmail.com>
2020-03-31 18:16:17 -0400

    initial commit

---
+++ assets/js/stopvideo.js
@@ -0,0 +1,7 @@
+$('.videoModal').on('hide.bs.modal', function(e) {    
+    var $if = $(e.delegateTarget).find('iframe');
+    var src = $if.attr("src");
+    $if.attr("src", '/empty.html');
+    $if.attr("src", src);
+});
+
