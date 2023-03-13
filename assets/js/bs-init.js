9578109103fef7e57eb05da34439954a0719bb75
Mat <matbook@MMIGNIN-MBP.local>
2023-03-13 03:07:05 -0400

    automatic upload

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -1,6 +1,11 @@
+if (window.innerWidth < 768) {
+	$('[data-bss-disabled-mobile]').removeClass('animated').removeAttr('data-aos data-bss-hover-animate data-bss-parallax-bg data-bss-scroll-zoom');
+}
+
 $(document).ready(function(){
-	$('[data-bs-hover-animate]')
-		.mouseenter( function(){ var elem = $(this); elem.addClass('animated ' + elem.attr('data-bs-hover-animate')) })
-		.mouseleave( function(){ var elem = $(this); elem.removeClass('animated ' + elem.attr('data-bs-hover-animate')) });
-	$('[data-bs-tooltip]').tooltip();
+
+	$('[data-bss-hover-animate]')
+		.mouseenter( function(){ var elem = $(this); elem.addClass('animated ' + elem.attr('data-bss-hover-animate')) })
+		.mouseleave( function(){ var elem = $(this); elem.removeClass('animated ' + elem.attr('data-bss-hover-animate')) });
+	$('[data-bss-tooltip]').tooltip();
 });
